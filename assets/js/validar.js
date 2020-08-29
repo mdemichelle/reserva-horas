4ad5d38d4567df05554b3c39c56f219739ef3dbe
michellemb306 <michellemirandab@gmail.com>
2020-08-29 15:56:18 -0400

    Validación edad y correo

--- assets/js/validar.js
+++ assets/js/validar.js
@@ -1,10 +1,5 @@
 //obteniendo los datos desde el ID
 var formulario = document.getElementById("formulario");
-var rut = document.getElementById("rut").value;
-var nombres = document.getElementById("nombres").value;
-var apellidos=document.getElementById("apellidos").value;
-var edad=document.getElementById("edad").value;
-var correo=document.getElementById("correo").value;
 var especialidad=document.getElementById("especialidad").value;
 var fecha=document.getElementById("fecha").value;
 var hora=document.getElementById("hora").value;
@@ -22,16 +17,14 @@ formulario.addEventListener("submit", e=>{
         return false;
     }else if(apellidoValidacion()==false){
         return false;
+    }else if(edadValidacion()==false)   {
+        return false;
+    }else if(correoValidacion()==false){
+        return false;
     } else{
         alert("su nombre: " +nombres + "rut: "+rut+"apellido: "+apellidos); 
         limpiar();
     }
-  
-    
-    
-  
-    
-    
 });
 
 //--------------------------------Validacion Rut: campo de texto. (validar su formato)------------------------------------------
@@ -41,53 +34,87 @@ rutValidacion = () => {
     var expresion = /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/gm;
 
         if(rut===""){
-            Swal.fire("El campo es obligatorio, por favor ingresa tu RUT");
+            Swal.fire("El campo es obligatorio, por favor ingresa un RUT.");
             return false;
 
         }else if(!rut.match(expresion)){
-            Swal.fire('El formato del RUT no es válido, por favor intenta nuevamente');
-            console.log(rut);
+            Swal.fire('El formato del RUT no es válido, por favor intenta nuevamente.');
+            
             
             return false;
         }
+        console.log("RUT: "+ rut);
         return true;
 }
 
 //----------------------------Validacion Nombre: campo de texto. (validar que contenga sólo letras)------------------------------------------
    nombreValidacion = () => {
     var nombres =document.getElementById("nombres").value;
-    var expresion = /^[a-zA-Z ]*$/;
+    var expresion = /^[a-zA-Z ]{3,35}$/;
     // var exp = /^[a-zA-Z]{3,15}\s[a-zA-Z]{3,15}$/gmi;
     if(nombres===""){
-        Swal.fire("El campo es obligatorio, por favor ingresa tus NOMBRES");
+        Swal.fire("El campo es obligatorio, por favor ingresa NOMBRES.");
         return false;
 
     }else if(!nombres.match(expresion)){
-        Swal.fire('El formato no es válido, por favor intenta nuevamente');
-        console.log(nombres);
+        Swal.fire('Debe ingresar sólo letras, por favor intenta nuevamente.');
+       
         return false;
     }
-    return true;
+        console.log("NOMBRES: "+ nombres);
+        return true;
 }
 
 //---------------------------Validación Apellidos: campo de texto. (validar que contenga sólo letras)---------
-apellidoValidacion = () => {
-    var apellidos=document.getElementById("apellidos").value;
-    var expresion = /^[a-zA-Z ]*$/;
-    if(apellidos===""){
-        Swal.fire("El campo es obligatorio, por favor ingresa tus APELLIDOS");
-        return false;
+    apellidoValidacion = () => {
+        var apellidos=document.getElementById("apellidos").value;
+        var expresion = /^[a-zA-Z ]{3,35}$/;
+        if(apellidos===""){
+            Swal.fire("El campo es obligatorio, por favor ingresa APELLIDOS.");
+            return false;
 
-    }else if(!apellidos.match(expresion)){
-        Swal.fire('El formato no es válido, por favor intenta nuevamente');
-        console.log(apellidos);
-        return false;
+        }else if(!apellidos.match(expresion)){
+            Swal.fire('Debe ingresar solo letras, por favor intenta nuevamente.');
+           
+            return false;
+        }
+        console.log("APELLIDOS: "+ apellidos);
+        return true;
     }
-    return true;
-}
 
+//----------------------- Validación Edad: campo de texto. (validar que se ingrese sólo números)------------------------
+    edadValidacion = () => {
+        var edad=document.getElementById("edad").value;
+        var expresion = /^\d{1,3}$/;
+        if(edad===""){
+            Swal.fire("El campo es obligatorio, por favor ingresa una EDAD.");
+            return false;
 
-//----------------------------Limpiar inputs-------------------------------------------------
+        }else if(!edad.match(expresion)){
+            Swal.fire('Debe ingresar sólo números, por favor intenta nuevamente.');
+            
+            return false;
+        }
+        console.log("EDAD: "+ edad);
+        return true;
+    }
+//---------------------------- Email: campo de texto. (validar su formato)--------------------------------------------
+    correoValidacion = () => {
+        var correo=document.getElementById("correo").value;
+        var expresion = /^\w+[@]\w+\.\D{2,3}$/;
+        if(correo===""){
+            Swal.fire("El campo es obligatorio, por favor ingresa un CORREO.");
+            return false;
+
+        }else if(!correo.match(expresion)){
+            Swal.fire('El formato del correo no es válido, por favor intenta nuevamente.');
+            return false;
+        }
+        console.log("CORREO: "+ correo);
+        return true;
+    }
+
+//----------------------------------------Limpiar inputs---------------------------------------------------
 limpiar = () =>{
     var rut = document.getElementById("rut");
     var nombres = document.getElementById("nombres");
